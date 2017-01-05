4cf1f085ae3a4c2c362d24cad581017c80b74d0d
Jai Kathuria <jaikathuria@live.com>
2017-01-05 13:28:04 +0530

    SideNav Added

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+
+$('.button-collapse').sideNav({
+    
+      menuWidth: 240, // Default is 240
+    
+      edge: 'left', // Choose the horizontal origin
+    
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+     
+    $('.ani-shadow').hover(function(){
+        
+        $(this).toggleClass('z-depth-4').toggleClass('z-depth-1');
+        
+    });
+
+ });
