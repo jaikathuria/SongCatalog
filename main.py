5b1fa66046bdba55467421969dfc6bcfd0abca2e
Jai Kathuria <jaikathuria@live.com>
2017-01-07 22:35:11 +0530

    Added local server File

---
+++ main.py
@@ -0,0 +1,21 @@
+# Imports Flask
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+
+# Import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_setup import Base, User, Genre, Songs
+# create engine connection with sql library
+engine = create_engine('sqlite:///MusicDatabase.db')
+#bind the engine with base class
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+       
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
