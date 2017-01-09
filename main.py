f4febb56724d8f925fecaca91f6cc87f31d0c610
Jai Kathuria <jaikathuria@live.com>
2017-01-09 23:43:33 +0530

    Added Gconnect Handler to main.py

--- main.py
+++ main.py
@@ -106,6 +106,61 @@ def viewSong(g_id,s_id):
     else:
         return redirect(url_for('genreListView',error = 'dataNotFound'))
   
+@app.route('/gconnect', methods = ['post'])
+def gconnect():
+    if request.args.get('state') != session['state']:
+        response.make_response(json.dumps('Invalid State paramenter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secret.json',scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response.make_response(json.dumps('Failed to upgrade the authorisation code'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    header = httplib2.Http()
+    result = json.loads(header.request(url, 'GET')[1])
+    
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    stored_credentials = session.get('credentials')
+    stored_gplus_id = session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    session['credentials'] = credentials
+    session['gplus_id'] = gplus_id
+    
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    
+    data = answer.json()
+
+    return data 
 
 def create_state():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
