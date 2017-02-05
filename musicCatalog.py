ed7c710c0fad657474808e50ec687814540b9d58
Jai Kathuria <jaikathuria@live.com>
2017-02-05 19:38:24 +0530

    Changed path for fb client secret

--- musicCatalog.py
+++ musicCatalog.py
@@ -251,7 +251,7 @@ def fbConnect():
     app_id = json.loads(open('/var/www/catalog/client_secret_fb.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('client_secret_fb.json', 'r').read())['web']['app_secret']
+        open('/var/www/catalog/client_secret_fb.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
