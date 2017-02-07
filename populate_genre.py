6c697958f71a208eec8d702c652f6bb8424b54b2
Jai Kathuria <jaikathuria@live.com>
2017-02-07 23:35:14 +0530

    Added comments to populate genre

--- populate_genre.py
+++ populate_genre.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 from db_setup import Base, User, Genre, Songs
 
+"""
+    Used to populate, the genre table.
+"""
 engine = create_engine('sqlite:///MusicDatabase.db')
 
 Base.metadata.bind = engine
@@ -11,7 +14,7 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
-genres = ['Folk', 'Patriotic', 'Ghazal', 'Classical', 'Westren', 'Filmi']
+genres = ['Folk', 'Patriotic', 'Ghazal', 'Classical', 'Western', 'Filmi']
 
 for name in genres:
     genreList = session.query(Genre).filter_by(name = name).one_or_none()
