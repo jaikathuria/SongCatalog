76fb0c7c88ec21e53857face9f689ad28b4daa28
Jai Kathuria <jaikathuria@live.com>
2017-02-08 19:06:00 +0530

    Bugs fixed main.py

--- db_setup.py
+++ db_setup.py
@@ -37,14 +37,14 @@ class Songs(Base):
     __tablename__ = 'songs'
 
     id = Column(Integer, primary_key=True)
-    name = name = Column(String(250), nullable = False)
+    name = Column(String(250), nullable = False)
     description = Column(String())
     url = Column(String(250),nullable = False)
     g_id = Column(Integer, ForeignKey('genre.id'))
     genre = relationship(Genre)
     u_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
-    
+
 
     @property
     def serialize(self):
